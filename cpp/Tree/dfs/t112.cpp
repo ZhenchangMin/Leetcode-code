0983cb597b96602d43e3fcf49e6e5240dffc58c8
241880334 <241880334@smail.nju.edu.cn>
2025-10-23 16:07:39 +0800

    Update codes.

---
+++ cpp/Tree/dfs/t112.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool hasPathSum(TreeNode *root, int targetSum)
+    {
+        if (!root)
+            return false;
+        if (root->val == targetSum && root->left == nullptr && root->right == nullptr)
+            return true;
+        bool left = false, right = false;
+        if (root->left != nullptr)
+            left = hasPathSum(root->left, targetSum - root->val);
+        if (root->right != nullptr)
+            right = hasPathSum(root->right, targetSum - root->val);
+        return left || right;
+    }
+};
