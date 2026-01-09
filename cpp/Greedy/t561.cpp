689d12a1690c665f828baf76580ef26e4e650383
ZhenchangMin <728262392@qq.com>
2026-01-09 20:20:17 +0800

    Update codes.

--- cpp/Greedy/t561.cpp
+++ cpp/Greedy/t561.cpp
@@ -1,8 +1,11 @@
-#include <bits/stdc++.h>
-using namespace std;
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-        
+        sort(nums.begin(), nums.end());
+        long long ans = 0;
+        for (int i = 0; i < nums.size(); i += 2){
+            ans += nums[i];
+        }
+        return ans;
     }
 };
