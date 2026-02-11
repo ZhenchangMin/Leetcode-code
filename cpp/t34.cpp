16aa5acaa60dc21124cb5aeb85c167db3d2a5b3c
ZhenchangMin <728262392@qq.com>
2026-02-12 07:59:04 +0800

    Update codes.

---
+++ cpp/t34.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        
+    }
+};
---
+++ cpp/t3679.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int minArrivalsToDiscard(vector<int>& arrivals, int w, int m) {
+        map<int, int> count;
+        int discarded = 0;
+        
+    }
+};
