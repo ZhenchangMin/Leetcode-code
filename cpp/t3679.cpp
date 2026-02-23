1585723a204aafa73cbac4c197e50e4d4a420173
ZhenchangMin <728262392@qq.com>
2026-02-23 17:10:06 +0800

    LC3679: double pointer and greedy.

--- cpp/t3679.cpp
+++ cpp/t3679.cpp
@@ -1,10 +1,30 @@
-#include <bits/stdc++.h>
+﻿#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
     int minArrivalsToDiscard(vector<int>& arrivals, int w, int m) {
-        map<int, int> count;
+        unordered_map<int, int> cnt;
+        int n = static_cast<int>(arrivals.size());
+        vector<int> kept(n, 0);
         int discarded = 0;
-        
+
+        for (int i = 0; i < n; ++i) {
+            int x = arrivals[i];
+
+            if (i >= w) {
+                int out = arrivals[i - w];
+                cnt[out] -= kept[i - w];
+            }
+
+            if (cnt[x] >= m) {
+                ++discarded;
+            } else {
+                kept[i] = 1;
+                ++cnt[x];
+            }
+        }
+
+        return discarded;
     }
 };
