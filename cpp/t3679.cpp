0ad0a9fcba3d542c45de8bdb5ea02ad6738fdfd6
ZhenchangMin <728262392@qq.com>
2026-02-23 19:03:08 +0800

    LC3679: add comments

--- cpp/t3679.cpp
+++ cpp/t3679.cpp
@@ -6,8 +6,8 @@ public:
     int minArrivalsToDiscard(vector<int>& arrivals, int w, int m) {
         unordered_map<int, int> cnt;
         int n = static_cast<int>(arrivals.size());
-        vector<int> kept(n, 0);
-        int discarded = 0;
+        vector<int> kept(n, 0); // use kept array to track if the arrival at index i is kept or discarded
+        int discarded = 0; // result
 
         for (int i = 0; i < n; ++i) {
             int x = arrivals[i];
